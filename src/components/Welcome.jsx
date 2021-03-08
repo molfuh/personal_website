31a20d712adb91fb386af9fe271457fa677ea24e
Molly Fuhrman <mollyfuhrman@Mollys-Air.lan1>
2021-03-07 19:11:18 -0800

    First draft done.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,44 +1,72 @@
-import styled from 'styled-components';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-  import Contact from './Contact.jsx'
+import styled, { css, keyframes } from 'styled-components';
+import { useState } from 'react';
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+//   } from "react-router-dom";
+//   import Contact from './Contact.jsx'
   
 
 export default function Welcome({ gif }) {
+  const [emailPop, useEmailPop] = useState(false);
+
+  const HandleEmail = () => {
+    navigator.clipboard.writeText('mollyfuhrman@gmail.com');
+    // useEmailPop(true);
+    useEmailPop(!emailPop);
+}
+
   return (
     <>
-    {console.log(navigator)}
       <StyledContainer />
       <StyledGif gif={gif}>
-        <Email onClick={() => navigator.clipboard.writeText('mollyfuhrman@gmail.com')}>mollyfuhrman@gmail.com</Email>
+        <Email onClick={() => HandleEmail()}>mollyfuhrman@gmail.com</Email>
+        <StyledEmailPopUp active={emailPop} />
+        {/* was going to do && trick here to render "clicked!" popup */}
+        {emailPop && <PopUp emailPop={emailPop}>
+          Copied to clipboard!    
+        </PopUp>}
         <GitHub href="https://github.com/molfuh" target="_blank">molfuh</GitHub>
         <LinkedIn href="https://www.linkedin.com/in/molly-fuhrman/" target="_blank">molly-fuhrman</LinkedIn>
-        <Router>
-          <Link to="/contact">
-              <ContactInfo>
-                  <Brackets>
-                  {`<`}
-                  </Brackets>
-                  Contact
-                  <Brackets>
-                  {`/>`}
-                  </Brackets>
-              </ContactInfo>
-            </Link>
-            <Switch>
-                <Route path="/contact">
-                  <Contact />
-                </Route>
-            </Switch>
-          </Router>
+        <ContactInfo>
+            <Brackets>
+            {`<`}
+            </Brackets>
+            Projects
+            <Brackets>
+            {` />`}
+            </Brackets>
+        </ContactInfo>
       </StyledGif>
     </>
   );
 }
+
+const StyledEmailPopUp = styled.div`
+`;
+
+const visibility = keyframes`
+  0% {opacity: 0}
+  25% {opacity: 1}
+  100% {opacity: 1}
+`;
+
+const PopUp = styled.div`
+  font-family: 'Menlo Regular';
+  font-weight: normal;
+  opacity: 0;
+  height: 10%;
+  width: 30%;
+  position: absolute;
+  /* background-color: ${props => props.emailPop && 'white' }; */
+  background-color: transparent;
+  color: yellowgreen;
+  animation: ${props => props.emailPop && css`${visibility} ease-in 0.5s` };
+  left: 30%;
+  top: 50%;
+`;
 
 const StyledContainer = styled.div`
   background-color: #1b181b;
@@ -70,6 +98,7 @@ const Email = styled.div`
   :hover {
       color: blue;
       cursor: pointer;
+
   }
 `;
 
