e1c8f2c9c4be9fc492c0e984800ad36ea6590f9d
molfuh <mollyfuhrman@gmail.com>
2023-01-30 10:03:07 -0800

    Added new website PositivelyTedious.com

--- src/components/ProjectListComponent.jsx
+++ src/components/ProjectListComponent.jsx
@@ -28,19 +28,19 @@ const Num = styled.li`
   color: #d09d81;
 `;
 
-let projectTitles=['INT Magazine','Meditative Musical Drum','Resume','title 4','title 5'];
+let projectTitles=['INT Magazine','Meditative Musical Drum','Positively Tedious','Resume','title 4','title 5'];
 let projectTitlesList=[];
 projectTitles.forEach((title,index)=>{
     projectTitlesList.push( <Num key={index}><span>{title}</span></Num>)
 })
 let projectDescription=['Online magazine highlighting creative themes and topics',
-'Minimum viable product for phone app to create self-designed meditative music','description 3','description 4','description 5'];
+'Minimum viable product for phone app to create self-designed meditative music','Portfolio website for new musician TEDIOUS to showcase important artist information','(Includes hidden surprise behind draggable logo!)','description 4','description 5'];
 let projectDescriptionList=[];
 projectDescription.forEach((description,index)=>{
     projectDescriptionList.push( <Num key={index}><span>{description}</span></Num>)
 })
-let projectURL=['http://www.intmagazine.com/',
-'https://meditativemusicaldrum.netlify.app/',Resume,'URL 4','URL 5'];
+let projectURL=['https://www.intmagazine.com/',
+'https://meditativemusicaldrum.netlify.app/','https://positivelytedious.com/',Resume,'URL 4','URL 5'];
 let projectURLList=[];
 projectURL.forEach((URL,index)=>{
     projectURLList.push( <Num key={index}><ProjectLink href={URL} target="_blank" rel="noreferrer">Click here to view!</ProjectLink></Num>)
@@ -57,12 +57,18 @@ function ProjectListComponent() {
         {projectDescriptionList[0]}
         {projectURLList[0]}
         <Num/>
+        {projectTitlesList[2]}
+        {projectDescriptionList[2]}
+        {projectDescriptionList[3]}
+        {projectURLList[2]}
+        <Num/>
         {projectTitlesList[1]}
         {projectDescriptionList[1]}
         {projectURLList[1]}
         <Num/>
-        {projectTitlesList[2]}
-        {projectURLList[2]}
+        <Num/>
+        {projectTitlesList[3]}
+        {projectURLList[3]}
         <Num/>
       </ListContainer>
     </>
